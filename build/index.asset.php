f9cfca8f791e6b4b64ed859eb464f1f17a7f380e
Erik <erikdmitchell@gmail.com>
2021-10-07 19:07:37 -0400

    blocks.json caused too many issues, this is stable
    long term blocks.json should be utalized

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '25cad403640d7b8dee106502ff2e7da3');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8e334725f053bfad8b0190855a087c06');
--- build/index.js
+++ build/index.js
@@ -472,7 +472,48 @@ Object(_wordpress_blocks__WEBPACK_IMPORTED_MODULE_0__["registerBlockType"])(name
   title: 'Home Grid',
   icon: 'editor-table',
   category: 'common',
-  attributes: {},
+  attributes: {
+    "postsToShow": {
+      "type": "number",
+      "default": 3
+    },
+    "excerptLength": {
+      "type": "number",
+      "default": 35
+    },
+    "columns": {
+      "type": "number",
+      "default": 2
+    },
+    "order": {
+      "type": "string",
+      "default": "desc"
+    },
+    "orderBy": {
+      "type": "string",
+      "default": "date"
+    },
+    "featuredImageSizeSlug": {
+      "type": "string",
+      "default": "digiwatt-home-grid"
+    },
+    "featuredImageSizeWidth": {
+      "type": "number",
+      "default": null
+    },
+    "featuredImageSizeHeight": {
+      "type": "number",
+      "default": null
+    },
+    "featuredImageLargeSizeSlug": {
+      "type": "string",
+      "default": "digiwatt-home-grid-large"
+    },
+    "featuredPostExcerptLength": {
+      "type": "number",
+      "default": 95
+    }
+  },
   edit: _edit__WEBPACK_IMPORTED_MODULE_1__["default"]
 });
 
