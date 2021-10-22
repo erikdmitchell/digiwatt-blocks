64f17fa080a9a621dde1cbc815794d865d64a855
Erik <erikdmitchell@gmail.com>
2021-10-22 15:26:02 -0400

    added supporting output funcs

--- blocks/post-header.php
+++ blocks/post-header.php
@@ -75,15 +75,66 @@ add_action( 'init', 'dwb_post_header_block_init' );
  * @return html
  */
 function render_block_digiwatt_post_header( $attributes ) {
-    $content = 'content';
+    global $post;
+    
+    $html = '';
 
+    $html .= '<header class="entry-header">';
+        $html .= '<div class="featured-columns">';
+            $html .= '<div class="featured-column">';
+                $html .= '<div class="header-content">';
+                    $html .= '<div class="title">';
+                        $html .= '<h1 class="entry-title">'.get_the_title( $post ).'</h1>';
+                    $html .= '</div>';
+                    $html .= '<div class="meta">';
+                        $html .= 'GET emdotbike_theme_posted_on();';
+                    $html .= '</div>';
+                $html .= '</div>';        
+            $html .= '</div>';
+            
+            if (has_post_thumbnail()) :
+                $html .= '<div class="featured-column">';
+                    $html .= get_dwb_post_header_post_thumbnail( $attributes['featuredImageSizeSlug'], $post->ID);
+                $html .= '</div>';
+            else :
+                $html .= '<div class="featured-column no-thumb"></div>';
+            endif;
+        $html .= '</div>';
+    $html .= '</header>';	   
+    
     $class = 'wp-block-dwb-post-header-block';
-
     $wrapper_attributes = get_block_wrapper_attributes( array( 'class' => $class ) );
 
     return sprintf(
         '<div %1$s>%2$s</div>',
         $wrapper_attributes,
-        $content,
+        $html,
     );
+}
+
+function get_dwb_post_header_posted_on( $show_author = false ) {
+    if ( is_sticky() && is_home() && ! is_paged() ) {
+        echo '<span class="featured-post">' . _e( 'Sticky', 'emdotbike' ) . '</span>';
+    }
+
+    // Set up and print post meta information. -- hide date if sticky.
+    if ( ! is_sticky() ) :
+        echo wp_kses_post( '<div class="entry-date"><a href="' . get_permalink() . '" rel="bookmark"><time class="entry-date" datetime="' . get_the_date( 'c' ) . '">' . get_the_date() . '</time></a></div>' );
+    endif;
+
+    echo wp_kses_post( '<div class="byline"><span class="author vcard"><a class="url fn n" href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" rel="author">By ' . get_the_author() . '</a></div></span>' );
+}
+
+function get_dwb_post_header_post_thumbnail( $size = 'full', $post_id = 0 ) {
+    $thumb = '';
+
+    if (has_post_thumbnail( $post_id )) {
+        $thumb_id = get_post_thumbnail_id( $post_id );
+        $thumb_src_url = wp_get_attachment_image_url( $thumb_id, $size );
+        $thumb_meta = wp_get_attachment_metadata( $thumb_id );
+        $thumb_base = '<img src="' . $thumb_src_url . '" class="img-responsive" />';
+        $thumb = wp_image_add_srcset_and_sizes( $thumb_base, $thumb_meta, $thumb_id );
+    }
+
+    return wp_kses_post( $thumb );
 }
