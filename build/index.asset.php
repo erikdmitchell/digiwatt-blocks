4ba62a366d25616234c9e7c8502c45e924a3a288
Erik Mitchell <erikdmitchell@gmail.com>
2022-07-13 14:02:32 -0400

    moved build to assets

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-wordcount'), 'version' => 'beaed2626ee8d8f7f7c3c9b351a70ffb');
